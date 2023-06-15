29c8ef9d986adfb465d70989bcd98033ea3a61a3
fuzhiqiang <fuzhiqiang@baidu.com>
2023-06-15 16:03:37 +0800

    1

--- MVVM原理完结/14. v-bind 表达式解析/core/util/code.js
+++
@@ -1,8 +0,0 @@
-// // generateCode
-
-// let flag = false;
-// if (obj.x < 2 + 1) {
-//   flag = true;
-// } else {
-//   flag = false;
-// }
--- MVVM原理完结/5. vnode/core/instance/mount.js
+++
@@ -1,8 +0,0 @@
-/**
- * 挂载函数
- * @param {*} vm
- * @param {*} el
- */
-export function mount(vm, el) {
-  console.log("begin mount");
-}
